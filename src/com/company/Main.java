fff500967d8463d3cec7f473635a49b572a43d1a
Ozi <voznesenskiy.evgeniy@mail.ru>
2019-09-24 18:50:41 +0300

    add comments

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,7 +5,7 @@ public class Main {
     private static String s1 = "приеду шестого";
     private static String s2 = " пгро ш  ес  и  ет од у  ";
 
-    static int[][] key = {
+    private static int[][] key = {
             {0, 1, 0, 1, 0},
             {0, 0, 0, 0, 0},
             {0, 0, 0, 1, 0},
@@ -14,11 +14,11 @@ public class Main {
     };
 
     public static void main(String[] args) {
-        System.out.println(encrypt(s1));
-        System.out.println(decrypt(s2));
+        System.out.println("Зашифрованное сообщение: " + encrypt(s1));
+        System.out.println("Расшифрованное сообщение: " + decrypt(s2));
     }
 
-    private static int[][] rotate90(int[][] arr) {
+    private static int[][] rotate90(int[][] arr) {  //функция поворота матрицы ключа на 90 градусов по часовой
         int[][] newArr = new int[arr[0].length][arr.length];
         for (int i = 0; i < arr.length; i++) {
             for (int j = 0; j < arr[i].length; j++) {
@@ -28,7 +28,7 @@ public class Main {
         return newArr;
     }
 
-    private static String encrypt(String s) {
+    private static String encrypt(String s) {  //функция шифрования
         char[] phrase = s.toCharArray();
         char[][] grille = new char[key.length][key[0].length];
         byte index = 0;
@@ -52,7 +52,7 @@ public class Main {
         return encryptedPhrase.toString();
     }
 
-    private static String decrypt(String s) {
+    private static String decrypt(String s) {   //функция расшифровки
         char[] phrase = s.toCharArray();
         char[][] grille = new char[key.length][key[0].length];
         for (int i = 0; i < grille.length; i++) {
